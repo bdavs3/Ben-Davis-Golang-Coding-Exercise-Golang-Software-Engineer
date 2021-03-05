cb15656d8e63d1bf9fdb3b3a04a99465e9b81e01
bdavs3 <bdavs3000@gmail.com>
2021-03-05 10:37:52 -0800

    Create binary_tree.go structure.
    - Set up object-oriented structure of the file.
    - Create struct for binary-tree with Root, Left, and Right.
    - Create String() function for binary trees.
    - Scaffold functions for Insert(), InOrder(), PreOrder(), and
      PostOrder().

--- main.go
+++ main.go
@@ -3,9 +3,18 @@ package main
 import (
 	"ayoconnect-golang-challenge/robber"
 	"ayoconnect-golang-challenge/tree"
+	"fmt"
 )
 
 func main() {
+	binaryTree := tree.NewBinaryTree(5)
+	fmt.Println(binaryTree)
+	binaryTree.Insert()
+	binaryTree.InOrder()
+	binaryTree.PreOrder()
+	binaryTree.PostOrder()
+
+	fmt.Println("")
+
 	robber.HouseRobber()
-	tree.BinaryTree()
 }
--- tree/binary_tree.go
+++ tree/binary_tree.go
@@ -2,6 +2,36 @@ package tree
 
 import "fmt"
 
-func BinaryTree() {
-	fmt.Println("Binary Tree")
+type BinaryTree struct {
+	Root  int
+	Left  *BinaryTree
+	Right *BinaryTree
+}
+
+func NewBinaryTree(root int) *BinaryTree {
+	return &BinaryTree{
+		Root:  root,
+		Left:  nil,
+		Right: nil,
+	}
+}
+
+func (bt *BinaryTree) String() string {
+	return fmt.Sprintf("I am a binary tree with root %d", bt.Root)
+}
+
+func (bt *BinaryTree) Insert() {
+	fmt.Println("Insert")
+}
+
+func (bt *BinaryTree) InOrder() {
+	fmt.Println("InOrder")
+}
+
+func (bt *BinaryTree) PreOrder() {
+	fmt.Println("PreOrder")
+}
+
+func (bt *BinaryTree) PostOrder() {
+	fmt.Println("PostOrder")
 }
