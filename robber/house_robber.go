7313dfee076ca90ca42657f57f2854853f225198
bdavs3 <bdavs3000@gmail.com>
2021-03-05 16:39:41 -0800

    Incorporate first and last house adjacency into robber logic

--- robber/house_robber.go
+++ robber/house_robber.go
@@ -12,21 +12,38 @@ func NewStreet(houses []int) *Street {
 
 func (s *Street) Rob() int {
 	numHouses := len(s.houses)
-	if numHouses == 0 {
-		return s.houses[0]
-	} else if numHouses == 0 {
-		return max(s.houses[0], s.houses[1])
-	} else {
-		maxAccumulatedValues := make([]int, numHouses)
-		maxAccumulatedValues[0] = s.houses[0]
-		maxAccumulatedValues[1] = max(s.houses[0], s.houses[1])
 
-		for i := 2; i < numHouses; i++ {
-			maxAccumulatedValues[i] = max(s.houses[i]+maxAccumulatedValues[i-2], maxAccumulatedValues[i-1])
-		}
-
-		return maxAccumulatedValues[numHouses-1]
+	if s.houses == nil || numHouses == 0 {
+		return 0
 	}
+	if numHouses == 1 {
+		return s.houses[0]
+	}
+
+	// To account for the circularity of the street, calculate values and take the max.
+	// max1 includes the first house and not the last, whereas max2 includes the last house and not the first.
+	max1 := calculateEarnings(s.houses, 0, numHouses-2)
+	max2 := calculateEarnings(s.houses, 1, numHouses-1)
+
+	return max(max1, max2)
+}
+
+func calculateEarnings(houses []int, start, end int) int {
+	numHouses := len(houses)
+
+	if start == end {
+		return houses[start]
+	}
+
+	bestValues := make([]int, numHouses)
+	bestValues[start] = houses[start]
+	bestValues[start+1] = max(houses[start], houses[start+1])
+
+	for i := start + 2; i <= end; i++ {
+		bestValues[i] = max(houses[i]+bestValues[i-2], bestValues[i-1])
+	}
+
+	return bestValues[end]
 }
 
 func max(x, y int) int {
