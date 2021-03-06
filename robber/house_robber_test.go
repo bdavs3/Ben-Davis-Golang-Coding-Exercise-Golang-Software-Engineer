49cdce636a6f84aa243db207cf287b81512d7ed6
bdavs3 <bdavs3000@gmail.com>
2021-03-06 14:51:36 -0800

    Add robber tests

--- robber/house_robber_test.go
+++ robber/house_robber_test.go
@@ -3,25 +3,45 @@ package robber
 import "testing"
 
 func TestRobber(t *testing.T) {
-	var tests = []struct {
+	tests := []struct {
 		comment string
-		want    string
+		houses  []int
+		want    int
 	}{
 		{
-			comment: "Clever",
-			want:    "Clever",
+			comment: "nil houses",
+			houses:  nil,
+			want:    0,
 		},
 		{
-			comment: "Sneaky",
-			want:    "Sneaky",
+			comment: "Houses (1)",
+			houses:  []int{1},
+			want:    1,
+		},
+		{
+			comment: "Houses (7, 3, 1, 5, 3)",
+			houses:  []int{7, 3, 1, 5, 3},
+			want:    12,
+		},
+		{
+			comment: "Houses (8, 11, 6, 40)",
+			houses:  []int{8, 11, 6, 40},
+			want:    51,
+		},
+		{
+			comment: "Houses (100, 5, 1, 103, 244, 24, 3, 77, 32, 100)",
+			houses:  []int{100, 5, 1, 103, 244, 24, 3, 77, 32, 100},
+			want:    426,
 		},
 	}
 
 	for _, test := range tests {
 		t.Run(test.comment, func(t *testing.T) {
-			trait := test.want
-			if trait != test.want {
-				t.Errorf("got %v, want %v", trait, test.want)
+			street := NewStreet(test.houses)
+			got := street.Rob()
+
+			if got != test.want {
+				t.Errorf("got %v, want %v", got, test.want)
 			}
 		})
 	}
